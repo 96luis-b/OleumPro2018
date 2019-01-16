df6d43d36e7814b4649bfd29e60e6c6bb13f23f6
96luis-b <luisbocaranda19@gmail.com>
2019-01-16 14:29:48 -0430

    send mail

---
+++ app/app.js
@@ -0,0 +1,42 @@
+
+      console.log(email);
+
+(function(){
+    emailjs.init("user_EGyMLxA34s0YlrWliR7p6");
+ })();
+
+
+ var myform = $("form#send_mail");
+ myform.submit(function(event){
+     event.preventDefault();
+ 
+     var params = myform.serializeArray().reduce(function(obj, item) {
+      obj[item.name] = item.value;
+      return obj;
+   }, {});
+ 
+   // Change to your service ID, or keep using the default service
+   var service_id = "send_email";
+ 
+   var template_id = "send_email";
+   myform.find("button").text("Sending...");
+   let data = {
+             message: message.value,
+             subject: 'prueba',
+             from_name: name.value,
+             from_email:email.value
+         };
+         console.log(data)
+         console.log(params);
+
+   emailjs.send(service_id, template_id, data)
+       .then(function(){ 
+        alert("Sent!");
+        myform.find("button").text("Send");
+      }, function(err) {
+        alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+        myform.find("button").text("Send");
+     });
+ 
+   return false;
+ });
