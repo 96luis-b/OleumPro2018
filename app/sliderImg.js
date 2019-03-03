ccd7a545e0cd2eaa198251164960866ac99bcd0a
96luis-b <luisbocaranda19@gmail.com>
2019-03-02 20:26:33 -0430

    slider

--- app/sliderImg.js
+++ app/sliderImg.js
@@ -6,24 +6,54 @@ var img4 = document.getElementById("img4");
 var img5 = document.getElementById("img5");
 */
 var header =  document.getElementById("header")
+var btn1 =  document.getElementById("btn1")
+var btn2 =  document.getElementById("btn2")
+var btn3 =  document.getElementById("btn3")
+var btn4 =  document.getElementById("btn4")
+var btn5 =  document.getElementById("btn5")
 
 function slider(index){
     console.log("hola loco");
     switch (index) {
         case 1:
             header.style.backgroundImage = "url('img/header.jpg')";
+            btn1.style.background = "white";
+            btn2.style.background = "rgba(255,255,255,.0)";
+            btn3.style.background = "rgba(255,255,255,.0)";
+            btn4.style.background = "rgba(255,255,255,.0)";
+            btn5.style.background = "rgba(255,255,255,.0)";
             break;
         case 2:
             header.style.backgroundImage = "url('img/placa.png')";
+            btn2.style.background = "white";
+            btn1.style.background = "rgba(255,255,255,.0)";
+            btn3.style.background = "rgba(255,255,255,.0)";
+            btn4.style.background = "rgba(255,255,255,.0)";
+            btn5.style.background = "rgba(255,255,255,.0)";
             break;
         case 3:
             header.style.backgroundImage = "url('img/confianza1.jpg')";
+            btn3.style.background = "white";
+            btn2.style.background = "rgba(255,255,255,.0)";
+            btn1.style.background = "rgba(255,255,255,.0)";
+            btn4.style.background = "rgba(255,255,255,.0)";
+            btn5.style.background = "rgba(255,255,255,.0)";
             break;
         case 4:
             header.style.backgroundImage = "url('img/planificacion.jpg')";
+            btn4.style.background = "white";
+            btn2.style.background = "rgba(255,255,255,.0)";
+            btn3.style.background = "rgba(255,255,255,.0)";
+            btn1.style.background = "rgba(255,255,255,.0)";
+            btn5.style.background = "rgba(255,255,255,.0)";
             break;
         case 5:
             header.style.backgroundImage = "url('img/confianza2.jpg')";
+            btn5.style.background = "white";
+            btn2.style.background = "rgba(255,255,255,.0)";
+            btn3.style.background = "rgba(255,255,255,.0)";
+            btn4.style.background = "rgba(255,255,255,.0)";
+            btn1.style.background = "rgba(255,255,255,.0)";
             break;
 
         default:
@@ -32,11 +62,14 @@ function slider(index){
 }
 
 
-window.addEventListener("load",myFunction());
+window.addEventListener("load",myFunction(1));
+btn1.style.background = "white";
+console.log("lol")
+
 var myVar;
-function myFunction() {
+function myFunction(i) {
     console.log("setTimeout active")
-    var i = 0;
+    console.log(btn1);
     myVar = setInterval(()=>{;
         
       if(i>5){
